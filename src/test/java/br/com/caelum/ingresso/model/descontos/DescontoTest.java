d1d30edb202e3837bdbee94ab207e5f38e135026
Samanta Fluture <samantafluture@gmail.com>
2021-04-05 20:58:16 -0300

    fazendo teste do ingresso

--- src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
+++ src/test/java/br/com/caelum/ingresso/model/descontos/DescontoTest.java
@@ -15,34 +15,32 @@ import br.com.caelum.ingresso.model.Sessao;
 
 public class DescontoTest {
 	
-	private Filme rogueOne;
-	private Sala sala3D;
-	private Sessao sessaoDasDez;
+	private Sessao sessao;
 
 	@Before
 	public void preparaSessoes() {
-		this.rogueOne = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
-		this.sala3D = new Sala("Sala 3D", new BigDecimal("20.5"));
-		this.sessaoDasDez = new Sessao(LocalTime.parse("10:00:00"), rogueOne, sala3D);
+		Filme filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
+		Sala sala = new Sala("Sala 3D", new BigDecimal("20.5"));
+		this.sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
 	}
 
 	@Test
 	public void naoDeveConcederDescontoParaIngressoNormal() {
-		Ingresso ingresso = new Ingresso(sessaoDasDez, new SemDesconto());
+		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
 		BigDecimal precoEsperado = new BigDecimal("32.50");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
 
 	@Test
 	public void deveConcederDescontoDe30PorcentoParaIngressosDeClientesDeBanco() {
-		Ingresso ingresso = new Ingresso(sessaoDasDez, new DescontoParaBancos());
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
 		BigDecimal precoEsperado = new BigDecimal("22.75");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
 
 	@Test
 	public void deveConcederDescontoDe50PorcentoParaIngressoDeEstudante() {
-		Ingresso ingresso = new Ingresso(sessaoDasDez, new DescontoParaEstudantes());
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
 		BigDecimal precoEsperado = new BigDecimal("16.25");
 		Assert.assertEquals(precoEsperado, ingresso.getPreco());
 	}
